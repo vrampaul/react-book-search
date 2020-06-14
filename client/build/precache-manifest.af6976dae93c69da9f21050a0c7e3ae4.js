7ddde40ac5d494134755944bd183210ddfb3a860
Vimal Rampaul <vrampaul@icloud.com>
2020-06-14 18:01:32 -0400

    first commit

---
+++ client/build/precache-manifest.af6976dae93c69da9f21050a0c7e3ae4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "53194a8b417d6d7c2457",
+    "url": "/static/js/main.72971c61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a4bc62eec01ec63b9cf9",
+    "url": "/static/js/2.495248d7.chunk.js"
+  },
+  {
+    "revision": "487538558a2d6a756a4bbaa38431a1c4",
+    "url": "/index.html"
+  }
+];
